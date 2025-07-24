aabdc6b3ebb87c0d1e7dd3697ee096795ecc2a98
cjgnass <cjgnass@gmail.com>
2025-07-23 20:41:35 -0400

    added DayButtons

--- app/(tabs)/habits.tsx
+++ app/(tabs)/habits.tsx
@@ -1,4 +1,13 @@
-import { ScrollView, StyleSheet, View, Text, Pressable } from "react-native";
+import {
+  Keyboard,
+  ScrollView,
+  StyleSheet,
+  TextInput,
+  View,
+  TouchableWithoutFeedback,
+  Text,
+  Pressable,
+} from "react-native";
 import { useState, useEfffect } from "react";
 import PlusCircleIcon from "@/components/PlusCircleIcon";
 import colors from "@/constants/colors";
@@ -10,6 +19,20 @@ export default function Habits() {
   const [addingHabit, setAddingHabit] = useState(false);
   const mainButtonSize = 50;
   const [loading, setLoading] = useState(false);
+  const [habit, setHabit] = useState("");
+  const [number, setNumber] = useState(0);
+  const [schedule, setSchedule] = useState("");
+
+  const handleBack = () => {
+    setAddingHabit(false);
+    setHabit("");
+    setNumber(0);
+    setSchedule("");
+  };
+
+  const handleAddHabit = () => {
+    console.log(`Adding Habit: ${habit}, ${number}, ${schedule}`);
+  };
 
   const render = () => {
     return !addingHabit ? (
@@ -31,8 +54,11 @@ export default function Habits() {
       </View>
     ) : (
       <View>
+        <View style={styles.addHabitHeaderContainer}>
+          <Text style={styles.addHabitHeader}>Add Habit </Text>
+        </View>
         <Pressable
-          onPress={() => setAddingHabit(false)}
+          onPress={handleBack}
           style={[
             styles.switchButton,
             {
@@ -44,16 +70,53 @@ export default function Habits() {
         >
           <BackIcon width={mainButtonSize} height={mainButtonSize} />
         </Pressable>
+        <View style={styles.addHabitBodyContainer}>
+          <View style={styles.addHabitContainer}>
+            <Text style={styles.addHabitText}>Habit : </Text>
+            <TextInput
+              value={habit}
+              onChangeText={setHabit}
+              style={styles.addHabitTextInput}
+            />
+          </View>
+
+          <View style={styles.addHabitContainer}>
+            <Text style={styles.addHabitText}>Number : </Text>
+            <TextInput
+              value={number}
+              onChangeText={setNumber}
+              style={styles.addHabitTextInput}
+              keyboardType="number-pad"
+            />
+          </View>
+          <View style={styles.addHabitContainer}>
+            <Text style={styles.addHabitText}>Schedule : </Text>
+            <TextInput
+              value={schedule}
+              onChangeText={setSchedule}
+              style={styles.addHabitTextInput}
+            />
+          </View>
+          <Pressable style={styles.confirmAddHabit} onPress={handleAddHabit}>
+            <Text style={styles.addHabitText}>Confirm</Text>
+          </Pressable>
+        </View>
       </View>
     );
   };
   return (
-    <View style={styles.container}>
-      <View style={styles.headerContainer}>
-        <Text style={styles.headerText}> Habits </Text>
+    <TouchableWithoutFeedback
+      onPress={Keyboard.dismiss}
+      accessable={false}
+      style={styles.container}
+    >
+      <View style={styles.container}>
+        <View style={styles.headerContainer}>
+          <Text style={styles.headerText}> Habits </Text>
+        </View>
+        <View style={styles.body}>{render()}</View>
       </View>
-      <View style={styles.body}>{render()}</View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -87,5 +150,42 @@ const styles = StyleSheet.create({
   body: {
     flex: 1,
     padding: 15,
+  },
+  addHabitHeaderContainer: {
+    padding: 15,
+  },
+  addHabitHeader: {
+    fontFamily: Fonts.bold,
+    fontSize: 40,
+  },
+  addHabitBodyContainer: {
+    margin: 40,
+    height: "85%",
+  },
+  addHabitText: {
+    fontFamily: Fonts.regular,
+    fontSize: 30,
+  },
+  addHabitContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginVertical: 30,
+  },
+  addHabitTextInput: {
+    height: "100%",
+    width: "50%",
+    borderColor: "black",
+    borderWidth: 2,
+    paddingHorizontal: 10,
+    borderRadius: 8,
+    marginBottom: 20,
+  },
+  confirmAddHabit: {
+    borderWidth: 2,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 10,
+    marginTop: 50,
+    borderRadius: 20,
   },
 });
