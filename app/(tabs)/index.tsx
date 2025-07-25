562c5c87947c01af9d6b6d49016f2cac3a426073
cjgnass <cjgnass@gmail.com>
2025-07-25 10:02:46 -0400

    general layout for index

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,36 +1,29 @@
-import { useCallback, useState, useEffect } from "react";
-import { ScrollView, View, StyleSheet, Text, Alert } from "react-native";
-import {
-  useAudioPlayer,
-  useAudioRecorder,
-  AudioModule,
-  RecordingPresets,
-  setAudioModeAsync,
-  useAudioRecorderState,
-} from "expo-audio";
-import {
-  useFonts,
-  Inter_400Regular,
-  Inter_700Bold,
-} from "@expo-google-fonts/inter";
-import { useFocusEffect } from "@react-navigation/native";
+import { StyleSheet, View, Text } from "react-native";
 import Fonts from "@/constants/fonts";
-import AssistantButton from "@/components/AssistantButton";
-import colors from "@/constants/colors";
-import Habit from "@/components/Habit";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function Home() {
-  const [fontsLoaded] = useFonts({
-    [Fonts.regular]: Inter_400Regular,
-    [Fonts.bold]: Inter_700Bold,
-  });
-
   return (
-    <View>
-      <Text> hello</Text>
+    <View style={styles.container}>
+      <View style={styles.topContainer}></View>
+      <View style={styles.upcomingContainer}></View>
+      <View style={styles.mainContainer}></View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  topContainer: {
+    flex: 1,
+    backgroundColor: "gray",
+  },
+  upcomingContainer: {
+    flex: 2,
+    backgroundColor: "black",
+  },
+  mainContainer: {
+    flex: 10,
+  },
+});
